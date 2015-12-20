f17b61d706ea9eaa77b222d7e2ad048b267883ca
mlink <marcel.link@t-online.de>
2015-12-20 21:47:21 +0100

    ^ remove unnecessary files & cleanup examples # repair broken functions after merge

--- README.md
+++ README.md
@@ -24,10 +24,8 @@ The password secuirty indicator is marked on 4 scores. These are
 
 Firstly include jQuery and the strength.css and strength.js files. Place these before `&lt;/head&gt;` section
 
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script type="text/javascript" src="strength.js"></script>
-	<script type="text/javascript" src="js.js"></script>
-					
 
 ##### Create a password input field
 
@@ -41,73 +39,27 @@ Once you have created your password input field you will need to initiate the pl
 
 At its most basic level you can initiate the plugin like:
 
-						
+
 	$(document).ready(function ($) {
 
         $("#myPassword").strength();
 
     });
-					
+
 
 If you want to initiate the plugin with options then you can do so like:
 
-								
+
 	$('#myPassword').strength({
             strengthClass: 'strength',
             strengthMeterClass: 'strength_meter',
             strengthButtonClass: 'button_strength',
             strengthButtonText: 'Show password',
-            strengthButtonTextToggle: 'Hide Password'
+            strengthButtonTextToggle: 'Hide Password',
+						showPasswordToggle: true,
+						veryWeakText: 'very weak',
+						weakText: 'weak',
+						mediumText: 'medium',
+						strongText: 'strong',
+						strengthText: 'Strength'
         });		
-
-#### ..:: Options
-
-<table>
-							<thead>
-								<tr>
-									<th>Variable</th>
-									<th>Default Value</th>
-									<th>Description</th>
-									<th>Valid Options</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>strengthClass</td>
-									<td>strength</td>
-									<td>The CSS class that you want your input field to have</td>
-									<td></td>
-								</tr>
-								<tr>
-									<td>strengthMeterClass</td>
-									<td>strength_meter</td>
-									<td>The CSS class that you want your input field to have</td>
-									<td></td>
-								</tr>
-								<tr>
-									<td>strengthButtonClass</td>
-									<td>button_strength</td>
-									<td>The CSS class that you want the toggle button to have</td>
-									<td></td>
-								</tr>
-								<tr>
-									<td>strengthButtonText</td>
-									<td>Show Password</td>
-									<td>The text that you want to show for the toggle button</td>
-									<td></td>
-								</tr>
-								<tr>
-									<td>strengthButtonTextToggle</td>
-									<td>Hide Password</td>
-									<td>The toggled text that you want to show for the toggle button</td>
-									<td></td>
-								</tr>
-								<tr>
-									<td>showPasswordToggle</td>
-									<td>true</td>
-									<td>Enable or disable the show password button</td>
-									<td>true or false</td>
-								</tr>
-
-							</tbody>
-						</table>
--- src/strength.js
+++ src/strength.js
@@ -4,145 +4,166 @@
  * Further changes, comments: @aaronlumsden
  * Licensed under the MIT license
  */
-;(function ( $, window, document, undefined ) {
+(function($, window, document, undefined) {
 
-    var pluginName = "strength",
-        defaults = {
-            strengthClass: 'strength',
-            strengthMeterClass: 'strength_meter',
-            strengthButtonClass: 'button_strength',
-            strengthButtonText: 'Show Password',
-            showPasswordToggle: true,
-            strengthButtonTextToggle: 'Hide Password',
-            veryWeakText: 'very weak',
-            weakText: 'weak',
-            mediumText: 'medium',
-            strongText: 'strong',
-            strengthText: 'Strength'
-        };
+  var pluginName = "strength",
+    defaults = {
+      strengthClass: 'strength',
+      strengthMeterClass: 'strength_meter',
+      strengthButtonClass: 'button_strength',
+      strengthButtonText: 'Show Password',
+      strengthButtonTextToggle: 'Hide Password',
+      showPasswordToggle: true,
+      veryWeakText: 'very weak',
+      weakText: 'weak',
+      mediumText: 'medium',
+      strongText: 'strong',
+      strengthText: 'Strength'
+    };
 
-       // $('<style>body { background-color: red; color: white; }</style>').appendTo('head');
+  // $('<style>body { background-color: red; color: white; }</style>').appendTo('head');
 
-    function Plugin( element, options ) {
-        this.element = element;
-        this.$elem = $(this.element);
-        this.options = $.extend( {}, defaults, options );
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
-    }
+  function Plugin(element, options) {
+    this.element = element;
+    this.$elem = $(this.element);
+    this.options = $.extend({}, defaults, options);
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.init();
+  }
 
-    Plugin.prototype = {
+  Plugin.prototype = {
 
-        init: function() {
-            var characters = 0;
-            var capitalletters = 0;
-            var loweletters = 0;
-            var number = 0;
-            var special = 0;
-            var self = this;
+    init: function() {
+      var characters = 0;
+      var capitalletters = 0;
+      var loweletters = 0;
+      var number = 0;
+      var special = 0;
+      var self = this;
 
-            var upperCase= new RegExp('[A-Z]');
-            var lowerCase= new RegExp('[a-z]');
-            var numbers = new RegExp('[0-9]');
-            var specialchars = new RegExp('([!,%,&,@,#,$,^,*,?,_,~])');
+      var upperCase = new RegExp('[A-Z]');
+      var lowerCase = new RegExp('[a-z]');
+      var numbers = new RegExp('[0-9]');
+      var specialchars = new RegExp('([!,%,&,@,#,$,^,*,?,_,~])');
 
-            function GetPercentage(a, b) {
-                return ((b / a) * 100);
-            }
+      function GetPercentage(a, b) {
+        return ((b / a) * 100);
+      }
 
-            function check_strength(thisval,thisid){
-            	if (thisval.length > 8) { characters = 1; } else { characters = -1; };
-            	if (thisval.match(upperCase)) { capitalletters = 1} else { capitalletters = 0; };
-            	if (thisval.match(lowerCase)) { loweletters = 1}  else { loweletters = 0; };
-                if (thisval.match(numbers)) { number = 1}  else { number = 0; };
-                if (thisval.match(specialchars)) { special = 1}  else { special = 0; };
-
-                var total = characters + capitalletters + loweletters + number + special;
-                var totalpercent = GetPercentage(7, total).toFixed(0);
-
-                if (!thisval.length) {total = -1;}
-
-                get_total(total,thisid);
-            }
-
-            function get_total(total,thisid){
-
-                var thismeter = $('div[data-meter="'+thisid+'"]');
-
-                thismeter.removeClass();
-                if (total <= 1) {
-                   thismeter.addClass('veryweak').html('very weak');
-                } else if (total == 2){
-                    thismeter.addClass('weak').html('weak');
-                } else if(total == 3){
-                    thismeter.addClass('weak').html('weak');
-                } else if(total == 4) {
-                    thismeter.addClass('medium').html('medium');
-                } else {
-                    thismeter.addClass('strong').html('strong');
-                }
-                if (total == -1) { thismeter.html(self.options.strengthText); }
-            }
-
-            var isShown = false;
-            var strengthButtonText = this.options.strengthButtonText;
-            var strengthButtonTextToggle = this.options.strengthButtonTextToggle;
-
-            var showPasswordToggle = this.options.showPasswordToggle;
-
-            var thisid = this.$elem.attr('id');
-
-            if (showPasswordToggle)
-                this.$elem.addClass(this.options.strengthClass).attr('data-password',thisid).after('<input style="display:none" class="'+this.options.strengthClass+' '+this.$elem.attr('class')+'" data-password="'+thisid+'" type="text" name="" value=""><a data-password-button="'+thisid+'" href="" class="'+this.options.strengthButtonClass+'">'+this.options.strengthButtonText+'</a><div class="'+this.options.strengthMeterClass+'"><div data-meter="'+thisid+'">Strength</div></div>');
-            else
-                this.$elem.addClass(this.options.strengthClass).attr('data-password',thisid).after('<input style="display:none" class="'+this.options.strengthClass+' '+this.$elem.attr('class')+'" data-password="'+thisid+'" type="text" name="" value=""><div class="'+this.options.strengthMeterClass+'"><div data-meter="'+thisid+'">Strength</div></div>');
-
-            this.$elem.bind('keyup keydown', function(event) {
-                var thisval = $('#'+thisid).val();
-                $('input[type="text"][data-password="'+thisid+'"]').val(thisval);
-                check_strength(thisval, thisid);
-            });
-
-            $('input[type="text"][data-password="'+thisid+'"]').bind('keyup keydown', function(event) {
-                var thisval = $('input[type="text"][data-password="'+thisid+'"]').val();
-                $('input[type="password"][data-password="'+thisid+'"]').val(thisval);
-                check_strength(thisval, thisid);
-            });
-
-            $(document.body).on('click', '.'+this.options.strengthButtonClass, function(e) {
-                e.preventDefault();
-                thisclass = 'hide_'+$(this).attr('class');
-
-                if (isShown) {
-                    $('input[type="text"][data-password="'+thisid+'"]').hide();
-                    $('input[type="password"][data-password="'+thisid+'"]').show().focus();
-                    $('a[data-password-button="'+thisid+'"]').removeClass(thisclass).html(strengthButtonText);
-                    isShown = false;
-                } else {
-                    $('input[type="text"][data-password="'+thisid+'"]').show().focus();
-                    $('input[type="password"][data-password="'+thisid+'"]').hide();
-                    $('a[data-password-button="'+thisid+'"]').addClass(thisclass).html(strengthButtonTextToggle);
-                    isShown = true;
-                }
-            });
-        },
-
-        yourOtherFunction: function(el, options) {
-            // some logic
+      function check_strength(thisval, thisid) {
+        if (thisval.length > 8) {
+          characters = 1;
+        } else {
+          characters = -1;
         }
-    };
+        if (thisval.match(upperCase)) {
+          capitalletters = 1;
+        } else {
+          capitalletters = 0;
+        }
+        if (thisval.match(lowerCase)) {
+          loweletters = 1;
+        } else {
+          loweletters = 0;
+        }
+        if (thisval.match(numbers)) {
+          number = 1;
+        } else {
+          number = 0;
+        }
+        if (thisval.match(specialchars)) {
+          special = 1;
+        } else {
+          special = 0;
+        }
 
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
-            }
-        });
-    };
+        var total = characters + capitalletters + loweletters + number + special;
+        var totalpercent = GetPercentage(7, total).toFixed(0);
 
-})( jQuery, window, document );
+        if (!thisval.length) {
+          total = -1;
+        }
 
+        get_total(total, thisid);
+      }
 
+      function get_total(total, thisid) {
+
+        var thismeter = $('div[data-meter="' + thisid + '"]');
+
+        thismeter.removeClass();
+        if(total == -1) {
+          thismeter.html(self.options.strengthText);
+        }else if (total <= 1) {
+          thismeter.addClass('veryweak').html(self.options.veryWeakText);
+        } else if (total == 2) {
+          thismeter.addClass('weak').html(self.options.weakText);
+        } else if (total == 3) {
+          thismeter.addClass('weak').html(self.options.weakText);
+        } else if (total == 4) {
+          thismeter.addClass('medium').html(self.options.mediumText);
+        } else {
+          thismeter.addClass('strong').html(self.options.strong);
+        }
+      }
+
+      var isShown = false;
+      var strengthButtonText = this.options.strengthButtonText;
+      var strengthButtonTextToggle = this.options.strengthButtonTextToggle;
+
+      var showPasswordToggle = this.options.showPasswordToggle;
+      var passwordToggle = "";
+
+      var thisid = this.$elem.attr('id');
+
+      if (showPasswordToggle) {
+        passwordToggle = '<a data-password-button="' + thisid + '" href="" class="' + this.options.strengthButtonClass + '">' + this.options.strengthButtonText + '</a>';
+      }
+
+      this.$elem.addClass(this.options.strengthClass).attr('data-password', thisid)
+          .after('<input style="display:none" class="' + this.options.strengthClass + ' ' + this.$elem.attr('class') + '" data-password="' +
+          thisid + '" type="text" name="" value="">'+passwordToggle+'<div class="' + this.options.strengthMeterClass + '"><div data-meter="' + thisid + '">'+this.options.strengthText+'</div></div>');
+
+      this.$elem.bind('keyup keydown', function(event) {
+        var thisval = $('#' + thisid).val();
+        $('input[type="text"][data-password="' + thisid + '"]').val(thisval);
+        check_strength(thisval, thisid);
+      });
+
+      $('input[type="text"][data-password="' + thisid + '"]').bind('keyup keydown', function(event) {
+        var thisval = $('input[type="text"][data-password="' + thisid + '"]').val();
+        $('input[type="password"][data-password="' + thisid + '"]').val(thisval);
+        check_strength(thisval, thisid);
+      });
+
+      $(document.body).on('click', '.' + this.options.strengthButtonClass, function(e) {
+        e.preventDefault();
+        var thisclass = 'hide_' + $(this).attr('class');
+
+        if (isShown) {
+          $('input[type="text"][data-password="' + thisid + '"]').hide();
+          $('input[type="password"][data-password="' + thisid + '"]').show().focus();
+          $('a[data-password-button="' + thisid + '"]').removeClass(thisclass).html(strengthButtonText);
+          isShown = false;
+        } else {
+          $('input[type="text"][data-password="' + thisid + '"]').show().focus();
+          $('input[type="password"][data-password="' + thisid + '"]').hide();
+          $('a[data-password-button="' + thisid + '"]').addClass(thisclass).html(strengthButtonTextToggle);
+          isShown = true;
+        }
+      });
+    }
+  };
+
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[pluginName] = function(options) {
+    return this.each(function() {
+      if (!$.data(this, "plugin_" + pluginName)) {
+        $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+      }
+    });
+  };
+
+})(jQuery, window, document);
